4a9dbd0a34d3704b82b3f54ef03417d3b62ca5c6
Said Santana <said.santana@iteso.mx>
2023-05-10 00:57:03 -0600

    News API

---
+++ tarea4/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const rutas=require('./rutas');
+
+
+const { engine } = require('express-handlebars'); 
+const port= 3000;
+const app=express();
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views','./src/views');
+
+rutas(app);
+
+app.get("*", function (req, res) {
+    res.status(404).send("Pagina no encontrada");
+});
+
+app.listen(port,()=> {
+    console.log('app is running in port ' + port);
+}); 
